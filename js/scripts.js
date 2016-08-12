0a1d92ca07a5ec1626fbdd5a57e02a0744a8836c
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-08-12 09:08:05 -0700

    add if/else branches in scripts.js for the subject question and results

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,34 @@ $(function() {
     var pastime = parseInt($(".pastime").val());
     var place = parseInt($(".place").val());
     var animal = parseInt($(".animal").val());
-    
+    if(subject === 1) {
+      rails = 1;
+      drupal = 1;
+      android = 1;
+      design = 1;
+      net = 1;
+    } else if (subject === 2) {
+        rails = 1;
+        drupal = 1;
+    } else if (subject === 3) {
+      android = 1;
+      design = 1;
+    } else {
+      drupal = 1;
+      net = 1;
+    }
+    if (rails === Math.max([rails, drupal, android, design, net])) {
+      $("body").text("Ruby/Rails!");
+    } else if (drupal === Math.max([rails, drupal, android, design, net])) {
+        $("body").text("PHP/Drupal!");
+    } else if (android === Math.max([rails, drupal, android, design, net])) {
+        $("body").text("Java/Android");
+    } else if (design === Math.max([rails, drupal, android, design, net])) {
+        $("body").text("CSS/Design!");
+    } else {
+        $("body").text("C#/.NET!");
+    }
+
 
 
   });
