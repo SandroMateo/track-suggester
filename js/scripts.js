ddc7492a71a59544cf98b995bcfcd0a1af54144e
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-08-12 08:25:09 -0700

    add javascript and jquery content that sets up needed variables and a submit method

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var rails; /*writing or arts or math*/
+    var drupal; /*writing or business or math*/
+    var android; /*math or arts*/
+    var design; /*arts or math*/
+    var net; /*business or math*/
+    var age = parseInt($("#age").val());
+    var subject = parseInt($(".subject").val());
+    var pastime = parseInt($(".pastime").val());
+    var place = parseInt($(".place").val());
+    var animal = parseInt($(".animal").val());
+    
+
+
+  });
+});
