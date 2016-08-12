f9c197a54511dd5e18bff67d05ae3f7f92f36f54
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-08-12 10:12:52 -0700

    add if/else branches for the rest of the questions and fix bugs of variables

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@ $(function() {
     var design; /*arts or math*/
     var net; /*business or math*/
     var age = parseInt($("#age").val());
-    var subject = parseInt($(".subject").val());
-    var pastime = parseInt($(".pastime").val());
-    var place = parseInt($(".place").val());
-    var animal = parseInt($(".animal").val());
+    var subject = parseInt($("input:radio[name=subject]:checked").val());
+    var pastime = parseInt($("input:radio[name=pastime]:checked").val());
+    var place = parseInt($("input:radio[name=place]:checked").val());
+    var animal = parseInt($("input:radio[name=animal]:checked").val());
     if(subject === 1) {
       rails = 1;
       drupal = 1;
@@ -27,19 +27,63 @@ $(function() {
       drupal = 1;
       net = 1;
     }
-    if (rails === Math.max([rails, drupal, android, design, net])) {
-      $("body").text("Ruby/Rails!");
-    } else if (drupal === Math.max([rails, drupal, android, design, net])) {
-        $("body").text("PHP/Drupal!");
-    } else if (android === Math.max([rails, drupal, android, design, net])) {
-        $("body").text("Java/Android");
-    } else if (design === Math.max([rails, drupal, android, design, net])) {
-        $("body").text("CSS/Design!");
+    if (pastime === 1) {
+      rails += 1;
+      drupal += 1;
+    } else if (pastime === 2) {
+        drupal += 1;
+        net += 1;
+    } else if (pastime === 3) {
+        drupal += 1;
+        android += 1;
+        design += 1;
+        net += 1;
     } else {
-        $("body").text("C#/.NET!");
+        rails += 1;
+        drupal += 1;
+        android += 1;
     }
-
-
+    if (place === 1) {
+      rails += 1;
+      android += 1;
+      design += 1;
+    } else if (place === 2) {
+      drupal += 1;
+      android += 1;
+      net += 1;
+      rails += 1;
+      design += 1;
+    } else if (place === 3) {
+      rails += 1;
+      android += 1;
+      design += 1;
+    } else {
+      android += 1;
+    }
+    if (animal === 1) {
+      rails += 1;
+      android += 1;
+      design += 1;
+    } else if (animal === 2) {
+        rails += 1;
+        drupal +=1;
+    } else if (animal === 3) {
+        drupal += 1;
+        net += 1;
+    } else {
+        android += 1;
+    }
+    if (rails > drupal && rails > android && rails > design && rails > net) {
+      $("#result").text("Ruby/Rails!");
+    } else if (drupal > rails && drupal > android && rails > design && rails > net) {
+        $("#result").text("PHP/Drupal!");
+    } else if (android > rails && android > drupal && android > design && android > net) {
+        $("#result").text("Java/Android");
+    } else if (design > rails && design > drupal && design > android && design > net) {
+        $("#result").text("CSS/Design!");
+    } else {
+        $("#result").text("C#/.NET!");
+    }
 
   });
 });
